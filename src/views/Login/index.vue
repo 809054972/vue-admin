1701a40151a87b44cbac26ad4c2e6c911cb43654
巴伐利亚药水哥 <809054972@qq.com>
2021-03-10 10:50:14 +0800

    ???

---
+++ src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="login">
+        this is Login
+        <el-button>moren</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login'
+}
+</script>
+
+<style lang="scss" scoped>  //不加scoped表示对全局有效，加了只对当前有效
+#login {
+    height: 100vh;  //vh指可视区，100vh满屏，50vh半屏
+    background-color: #344a5f;
+}
+</style>
